d7ed6b403fb474bb585d1572ad432fad70a31910
Jillian Burke <jillian.burke@rafflebox.ca>
2024-12-10 21:36:51 -0400

    minor updates to a few things

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
+
+import vue from "@astrojs/vue";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), vue(), icon()],
+  env: {
+    schema: {
+      HARDCOVER_API_URL: envField.string({
+        context: "client",
+        access: "public",
+        optional: true,
+      }),
+      HARDCOVER_API_KEY: envField.string({
+        context: "server",
+        access: "secret",
+      }),
+    },
+  },
 });
